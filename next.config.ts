a49c237c5cc1924b8dec5731a88eb753d9902b1d
Enzo Patti <enzo.patti@aluno.senai.br>
2025-10-18 16:16:08 -0300

    images export updated to work in deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ output: "export",
+
+ // GitHub Pages deployment
+ basePath: process.env.NODE_ENV === "production" ? "/terralsocial" : "",
+ assetPrefix: process.env.NODE_ENV === "production" ? "/terralsocial/" : "",
+
+ // Disable image optimization for static export
+ images: {
+  unoptimized: true,
+ },
+
+ // Ensure trailing slashes
+ trailingSlash: true,
 };
 
 export default nextConfig;
